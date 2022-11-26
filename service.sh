77e737efb6217ea63d125a3e1bd12143cb2b7e40
荀攸即 <101364247+X-yael@users.noreply.github.com>
2022-11-27 01:55:25 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,7 +1,8 @@
 #!/system/bin/sh
+#该文件在/data/adb/modules/Alist
 MODDIR=${0%/*}
+#程序赋权，过后可删
 chmod 777 $MODDIR/alist
-$MODDIR/alist server --conf $MODDIR/data/config.json&
-sleep 60
-kill $(pgrep alist)
-$MODDIR/alist server --conf $MODDIR/data/config.json&
+#一分钟后启动，避免无网络导致失败
+#启动命令
+$MODDIR/alist server --data $MODDIR/data&
